361135df6d6eb126e087928dcbd0ce6302ac247e
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-08-29 12:59:32 +0300

    finished

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-import s from './Contacts.module.css'
+import s from './Contacts.module.css';
 
-const Contacts = ({ removeContact ,getFilteredConatcts, handleFilter }) => {
+const Contacts = ({ removeContact, getFilteredConatcts, handleFilter }) => {
   const elements = getFilteredConatcts.map(({ id, name, number }) => (
     <li key={id}>
-      {name}: {number} <button className={s.btn} onClick={() => removeContact(id)}>Delete</button>
+      {name}: {number}{' '}
+      <button className={s.btn} onClick={() => removeContact(id)}>
+        Delete
+      </button>
     </li>
   ));
 
@@ -13,7 +16,8 @@ const Contacts = ({ removeContact ,getFilteredConatcts, handleFilter }) => {
     <>
       <label className={s.label}>
         Find contacts by name
-        <input className={s.input}
+        <input
+          className={s.input}
           type="text"
           name="filter"
           placeholder="enter the name"
@@ -27,8 +31,8 @@ const Contacts = ({ removeContact ,getFilteredConatcts, handleFilter }) => {
 
 Contacts.propTypes = {
   removeContact: PropTypes.func.isRequired,
-  getFilteredConatcts: PropTypes.array,
+  getFilteredConatcts: PropTypes.func.isRequired,
   handleFilter: PropTypes.func.isRequired,
-}
+};
 
 export default Contacts;
